8283be51e5e93ccf17920efbabee640b57890bf3
Heilly <verolopezq0516@gmail.com>
2025-10-21 19:41:12 -0500

    "evento"

--- ferias/src/main/java/com/senasoft/ferias/serviceimpl/EventoServiceImpl.java
+++ ferias/src/main/java/com/senasoft/ferias/serviceimpl/EventoServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EventoServiceImpl implements EventoService {
 
+    //LLlamado al repositorio
     @Autowired
     private EventoRepository eventoRepository;
 
